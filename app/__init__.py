b5e4703710b5f494a6b4a932bf13e1fa18c71a5f
Mihail <168080573+ExShare074@users.noreply.github.com>
2025-05-19 21:27:30 +0300

    Использование конфигурации из config.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from app.config import Config
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'my secret key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
