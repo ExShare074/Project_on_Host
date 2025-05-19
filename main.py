c62fd314c8b01753bc2cd6117c09b57c65004f4d
Mihail <168080573+ExShare074@users.noreply.github.com>
2025-05-19 21:30:33 +0300

    Добавлено логирование в файл logs/app.log

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from app import app, db
 from app import routes, models
+import logging
+from logging.handlers import RotatingFileHandler
+import os
 
 with app.app_context():
     db.create_all()
+
+if not os.path.exists('logs'):
+    os.mkdir('logs')
+file_handler = RotatingFileHandler('logs/app.log', maxBytes=10240, backupCount=1)
+file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+file_handler.setLevel(logging.INFO)
+app.logger.addHandler(file_handler)
+
+app.logger.setLevel(logging.INFO)
+app.logger.info('Приложение запущено')
 
 if __name__ == "__main__":
     app.run()
